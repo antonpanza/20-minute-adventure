9b26eda1af23969ca5593f3e05433f0628188942
Anton Panza <56302827+antonpanza@users.noreply.github.com>
2025-02-11 22:39:00 +0200

    feat: Add mobile responsiveness

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -7,9 +7,9 @@ const CharacterCard = ({ character, index }) => {
             href={character.url}
             target="_blank"
             className={`shadow-lg py-5 px-8 flex flex-col w-full ${index < 2 ? "md:w-[calc(50%-10px)]" : "md:w-[calc(33%-10px)]"} grow transition-transform transform hover:scale-105`}>
-            <h2 className={`text-[25px] leading-none ${index < 2 ? "mb-32" : "mb-14"}`}>{character.name}</h2>
+            <h2 className={`text-[25px] leading-none mb-5 md:${index < 2 ? "mb-32" : "mb-14"}`}>{character.name}</h2>
 
-            <div className="mt-auto flex justify-between items-center">
+            <div className="mt-auto flex flex-col sm:flex-row sm:justify-between sm:items-center">
                 <div className="flex items-center text-[14px]">
                     <span className="text-gray-600 mr-1">Status:</span>
                     <span className={`font-bold ${
